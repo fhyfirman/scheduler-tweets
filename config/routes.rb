83ba3b2af858120f03c7f05f41d030250085e055
fhyfirman <firman@hungryhub.com>
2023-01-24 10:41:38 +0700

    create routes for about us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # GET /about
+  get "about-us", to: "about#index", as: :about
+
+  # GET root URL
+  # get "/", to: "main#index"
+  root to: "main#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
