b738f27d94ac57ab4d6fd0f92b59b7ca15b247ee
fhyfirman <firman@hungryhub.com>
2023-01-25 12:08:16 +0700

    add edit password feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: "main#index"
   get "about-us", to: "about#index", as: :about
 
+  # edit password
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   # user page
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
